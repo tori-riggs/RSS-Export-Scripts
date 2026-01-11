112c0fe82f1102d347225e74f4118bbacd30f126
Tori R <vmr7783@rit.edu>
2026-01-11 13:13:40 -0500

    Starred and saved search modes added.

--- feedbin.py
+++ feedbin.py
@@ -14,7 +14,7 @@ current_datetime = datetime.now()
 date = current_datetime.strftime("%Y%m%d%H%M")
 path = (f"{path}")
 
-def read_file(filename):
+def read_file(filename, starred, mode):
     with open(filename) as file:
         for line in file:
             if (not line.startswith("#") and line.strip()):
@@ -23,19 +23,26 @@ def read_file(filename):
                 title = fields[1].lstrip().strip("\"")
                 start = int(fields[2])
                 end = int(fields[3])
-                write(feed_id, title, start, end)
+                write(feed_id, title, start, end, starred, mode)
 
 
-def write(feed_id, title, start, end):
-    end = end + 1 # inclusive end
+def write(feed_id, title, start, end, starred, mode):
     all_data = []
     print(f"\nExporting: '{title}' (pages {start} - {end})")
     file_name = (os.path.join(path, f"{date} Feedbin {title}.json"))
-    # file_name = (os.path.join(path, f"{date} Feedbin {title} starred.json"))
+    if starred:
+        file_name = (os.path.join(path, f"{date} Feedbin {title} starred.json"))
 
     with open(file_name, "w") as f:
-        for page_number in range(start, end + 1):
-            url = f"https://api.feedbin.com/v2/feeds/{feed_id}/entries.json?page={page_number}"
+        for page_number in range(start, end + 1): # inclusive end
+            url = ""
+            if mode == 0:
+                # default behavior: feeds mode
+                url = f"https://api.feedbin.com/v2/feeds/{feed_id}/entries.json?page={page_number}"
+            elif mode == 1: # saved search mode
+                url = f"https://api.feedbin.com/v2/saved_searches/{feed_id}.json?include_entries=true&page={page_number}"
+            if starred: # if starred
+                url = url + "&starred=true"
             # starred:
             # url = f"https://api.feedbin.com/v2/feeds/{feed_id}/entries.json?starred=true&page={page_number}"
             # saved search:
@@ -62,15 +69,23 @@ def write(feed_id, title, start, end):
         f.write(json.dumps(all_data, indent=4, ensure_ascii=False))
 
 def main():
+    global path
     parser = argparse.ArgumentParser(prog="feedbin.py")
-    parser.add_argument("-o", "--output", help="Output path directory", action='store', default=path)
+    parser.add_argument("-o", "--output", metavar="path", help="Output path directory", action='store', default=path)
     parser.add_argument("-c", "--config", help="Path to config file", default="feedbin_config.py")
     parser.add_argument("-f", "--feeds", help="Path to list of feeds", default=feeds)
+    parser.add_argument("-s", "--starred", help="Export starred articles only", action="store_true")
+    parser.add_argument("-u", "--url", help="URL type: feed or saved search", choices=["feed", "search", "saved_search"], default="feed")
     args = parser.parse_args()
+    path = args.output
     print(args.output)
     print(args.config)
-    print(args.feed)
-    read_file(args.config)
-    read_file(args.feeds)
+    print(args.feeds)
+    starred = args.starred
+    mode = 0 # mode = 0 is default feed exporting
+    if args.url == "search" or args.url == "saved_search":
+        mode = 1 # mode = 1 is saved search exporting
+    # read_file(args.config)
+    read_file(args.feeds, starred, mode)
 
 main()
